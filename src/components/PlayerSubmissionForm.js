40d2aecf6cc038c8a47efda9406f354bdd3ec445
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-22 16:54:27 -0700

    adds text fields, updates styling

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,21 +1,81 @@
 import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+  
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{props.player}</h3>
 
       <form className="PlayerSubmissionForm__form" >
 
         <div className="PlayerSubmissionForm__poem-inputs">
 
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
-          <input
-            placeholder="hm..."
-            type="text" />
+          <div>
+            <span>{props.fields[0]}</span>
+          </div>
+
+          <div>
+            <label htmlFor={props.fields[1].key}>{props.fields[1].placeholder}</label>
+              <input 
+                name={props.fields[1].key} 
+                placeholder={props.fields[1].placeholder}
+                value={props.fields[1].value}
+              />
+          </div>
+
+          <div>
+            <label htmlFor={props.fields[2].key}>{props.fields[2].placeholder}</label>
+              <input 
+                name={props.fields[2].key} 
+                placeholder={props.fields[2].placeholder}
+                value={props.fields[2].value}
+              />
+          </div>
+
+          <div>
+            <label htmlFor={props.fields[3].key}>{props.fields[3].placeholder}</label>
+              <input 
+                name={props.fields[3].key} 
+                placeholder={props.fields[3].placeholder}
+                value={props.fields[3].value}
+              />
+          </div>
+
+          <div>
+            <label htmlFor={props.fields[4].key}>{props.fields[4].placeholder}</label>
+              <input 
+                name={props.fields[4].key} 
+                placeholder={props.fields[4].placeholder}
+                value={props.fields[4].value}
+              />
+          </div>
+
+          <div>
+            <span>{props.fields[5]}</span>
+          </div>
+
+          <div>
+            <label htmlFor={props.fields[6].key}>{props.fields[6].placeholder}</label>
+              <input 
+                name={props.fields[6].key} 
+                placeholder={props.fields[6].placeholder}
+                value={props.fields[6].value}
+              />
+          </div>
+
+          <div>
+            <label htmlFor={props.fields[7].key}>{props.fields[7].placeholder}</label>
+              <input 
+                name={props.fields[7].key} 
+                placeholder={props.fields[7].placeholder}
+                value={props.fields[7].value}
+              />
+          </div>
+
+          <div>
+            <span>{props.fields[8]}</span>
+          </div>
 
         </div>
 
