5f4388a9dafff606385f8dc6248ceb86200bbbcd
peachmakkoli <sara.lee.higgins@gmail.com>
2020-04-22 23:21:36 -0700

    hides RecentSubmission and PlayerSubmissionForm when the final poem is revealed

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = ({ player, onLineSubmitCallback }) => {
+const PlayerSubmissionForm = ({ player, onLineSubmitCallback, revealPoem }) => {
 
   const [ fieldInput, setFieldInput ] = useState({
       adj1: '',
@@ -49,99 +49,103 @@ const PlayerSubmissionForm = ({ player, onLineSubmitCallback }) => {
     };
   };
 
-  return (
-    <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{player}</h3>
+  if (revealPoem) {
+    return null;
+  } else {
+    return (
+      <div className="PlayerSubmissionForm">
+        <h3>Player Submission Form for Player #{player}</h3>
 
-      <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit}>
+        <form className="PlayerSubmissionForm__form" onSubmit={onFormSubmit}>
 
-        <div className="PlayerSubmissionForm__poem-inputs">
+          <div className="PlayerSubmissionForm__poem-inputs">
 
-          <div>
-            <span>The</span>
+            <div>
+              <span>The</span>
+            </div>
+
+            <div>
+              <label htmlFor="adj1">adjective</label>
+                <input 
+                  name="adj1" 
+                  placeholder="adjective"
+                  value={fieldInput.adj1}
+                  onChange={onInputChange}
+                />
+            </div>
+
+            <div>
+              <label htmlFor="noun1">noun</label>
+                <input 
+                  name="noun1" 
+                  placeholder="noun"
+                  value={fieldInput.noun1}
+                  onChange={onInputChange}
+                />
+            </div>
+
+            <div>
+              <label htmlFor="adv">adverb</label>
+                <input 
+                  name="adv" 
+                  placeholder="adverb"
+                  value={fieldInput.adv}
+                  onChange={onInputChange}
+                />
+            </div>
+
+            <div>
+              <label htmlFor="verb">verb</label>
+                <input 
+                  name="verb" 
+                  placeholder="verb"
+                  value={fieldInput.verb}
+                  onChange={onInputChange}
+                />
+            </div>
+
+            <div>
+              <span>the</span>
+            </div>
+
+            <div>
+              <label htmlFor="adj2">adjective</label>
+                <input 
+                  name="adj2"
+                  placeholder="adjective"
+                  value={fieldInput.adj2}
+                  onChange={onInputChange}
+                />
+            </div>
+
+            <div>
+              <label htmlFor="noun2">noun</label>
+                <input 
+                  name="noun2" 
+                  placeholder="noun"
+                  value={fieldInput.noun2}
+                  onChange={onInputChange}
+                />
+            </div>
+
+            <div>
+              <span>.</span>
+            </div>
+
           </div>
 
-          <div>
-            <label htmlFor="adj1">adjective</label>
-              <input 
-                name="adj1" 
-                placeholder="adjective"
-                value={fieldInput.adj1}
-                onChange={onInputChange}
-              />
+          <div className="PlayerSubmissionForm__submit">
+            <input 
+              type="submit" 
+              value="Submit Line" 
+              className="PlayerSubmissionForm__submit-btn"
+            />
           </div>
-
-          <div>
-            <label htmlFor="noun1">noun</label>
-              <input 
-                name="noun1" 
-                placeholder="noun"
-                value={fieldInput.noun1}
-                onChange={onInputChange}
-              />
-          </div>
-
-          <div>
-            <label htmlFor="adv">adverb</label>
-              <input 
-                name="adv" 
-                placeholder="adverb"
-                value={fieldInput.adv}
-                onChange={onInputChange}
-              />
-          </div>
-
-          <div>
-            <label htmlFor="verb">verb</label>
-              <input 
-                name="verb" 
-                placeholder="verb"
-                value={fieldInput.verb}
-                onChange={onInputChange}
-              />
-          </div>
-
-          <div>
-            <span>the</span>
-          </div>
-
-          <div>
-            <label htmlFor="adj2">adjective</label>
-              <input 
-                name="adj2"
-                placeholder="adjective"
-                value={fieldInput.adj2}
-                onChange={onInputChange}
-              />
-          </div>
-
-          <div>
-            <label htmlFor="noun2">noun</label>
-              <input 
-                name="noun2" 
-                placeholder="noun"
-                value={fieldInput.noun2}
-                onChange={onInputChange}
-              />
-          </div>
-
-          <div>
-            <span>.</span>
-          </div>
-
-        </div>
-
-        <div className="PlayerSubmissionForm__submit">
-          <input 
-            type="submit" 
-            value="Submit Line" 
-            className="PlayerSubmissionForm__submit-btn"
-          />
-        </div>
-      </form>
-    </div>
-  );
-}
+        </form>
+      </div>
+    );
+  };
+};
 
 
 export default PlayerSubmissionForm;
--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './RecentSubmission.css';
 
-const RecentSubmission = ({ line }) => {
-  console.log(line);
-  return (
-    <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{line}</p>
-    </div>
-  );
-}
+const RecentSubmission = ({ line, revealPoem }) => {
+  if (!line || revealPoem) {
+    return null;
+  } else {
+    return (
+      <div className="RecentSubmission">
+        <h3>The Most Recent Submission</h3>
+        <p className="RecentSubmission__submission">{line}</p>
+      </div>
+    );
+  };
+};
 
 export default RecentSubmission;
